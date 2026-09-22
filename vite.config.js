6525acf1c488aba8e5800fed1b1db93e6c5ab30c
Tristan <tristan.tempelmann@tu-dortmund.de>
2026-09-22 17:00:45 +0200

    Switched to clingo and ASP

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   base: "/enclose-horse-solver/",
 
-  server: {
-    headers: {
-      "Cross-Origin-Opener-Policy": "same-origin",
-      "Cross-Origin-Embedder-Policy": "require-corp",
-    },
-  },
-
-  preview: {
-    headers: {
-      "Cross-Origin-Opener-Policy": "same-origin",
-      "Cross-Origin-Embedder-Policy": "require-corp",
-    },
+  worker: {
+    format: "es",
   },
 });
