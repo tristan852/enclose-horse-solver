a61be388f28c3703a10c58efe802970fb7b401cc
Tristan <tristan.tempelmann@tu-dortmund.de>
2026-09-25 18:09:52 +0200

    Update solver.js

--- src/solver.js
+++ src/solver.js
@@ -641,7 +641,7 @@ export function waterGetsWave(x, y, width) {
     const w = Math.sin(24690 + index * 67890) * 1e4;
     const v = w - Math.floor(w);
 
-    return v < 0.7;
+    return v < 0.06;
 }
 
 function resizeBoard(card, width, height) {
