adde4541e7c07a0786e8c8c4e57cefb5f2890761
Tristan <tristan.tempelmann@tu-dortmund.de>
2026-09-26 15:02:16 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,7 +2,11 @@ const isEdit = window.location.pathname
   .replace(/\/$/, "")
   .endsWith("/edit");
 
-if (isEdit) {
+const isPlay = window.location.pathname
+  .replace(/\/$/, "")
+  .endsWith("/play");
+
+if (isEdit || isPlay) {
   import("./editor.js");
 } else {
   import("./solver.js");
